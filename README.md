d0d2883ed21a488e487ae386ea320651e5db5119
Konstantin Komelin <konstantin.komelin@gmail.com>
2016-09-29 13:28:54 +0300

    Uploaded the module, license and readme.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# qrcoder
-Drupal 8 modulem which was developed for demo purposes and it is an addition to my article.
+# QRCoder
+Drupal 8 module that was developed for demo purposes and it is an addition to my article.
+
+Please note that QRCoder module is not for production use and provided "AS IS".
---
+++ src/Plugin/Field/FieldFormatter/QRCodeLinkFieldFormatter.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\qrcoder\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'qrcode_link_field_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "qrcode_link_field_formatter",
+ *   label = @Translation("QR Code"),
+ *   field_types = {
+ *     "link"
+ *   }
+ * )
+ */
+class QRCodeLinkFieldFormatter extends FormatterBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      // Implement default settings.
+    ) + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return array(
+      // Implement settings form.
+    ) + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      // Get a URL from the field and make it absolute.
+      $url = $item->getUrl()->setAbsolute(TRUE)->toString();
+      // Just in case, sanitize entered URL.
+      $url =  \Drupal\Component\Utility\UrlHelper::filterBadProtocol($url);
+      // Use image theme function to render a QR code image for the URL.
+      $elements[$delta] = [
+        '#theme' => 'image',
+        '#uri' => 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=' . $url,
+      ];
+    }
+
+    return $elements;
+  }
+
+}
